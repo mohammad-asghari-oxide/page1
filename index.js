a2708c35f63d83144dbb91e8530545dbfcdb66d8
mohammad-asghari-oxide <90542547+mohammad-asghari-oxide@users.noreply.github.com>
2026-04-10 17:04:56 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/data', async (req, res) => {
+  const url = 'http://example.com/'; // Replace with the actual URL
+  const response = await axios.get(url);
+  res.send(response.data);
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
